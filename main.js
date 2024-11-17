6d0713e15aa1365a097d37cd977a5dc50f2f325c
farandaa <aranda.fabiaan@gmail.com>
2024-11-17 17:00:01 -0300

    agrego boton de scroll al inicio

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+const botonFijo = document.getElementById('botonFijo');
+const posicion = 200;
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > posicion) {
+        botonFijo.style.display = 'block';
+    } else {
+        botonFijo.style.display = 'none';
+    }
+});
+
 function scrollCards(direction, cardsContainer) {
     const scrollAmount = 200;
-    cardsContainer.scrollBy({
-        left: direction * scrollAmount,
-        behavior: 'smooth'
-    });
+    cardsContainer.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
 }
-  
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
